47eef8c5e35f617774bafddc99de7d0028560706
Connor McDermid <cam-o-man@live.com>
2019-07-12 23:57:08 -0400

    documentation
    Signed-off-by: Connor McDermid <cam-o-man@live.com>

--- app/src/main/java/org/hackSugar/hopper/HSTileService.java
+++ app/src/main/java/org/hackSugar/hopper/HSTileService.java
@@ -1,13 +1,33 @@
+/* Copyright (c) 2019 Tejas Mehta, Frank Gomes, Connor McDermid.
+* Licensed under the GNU General Public License version 3.0
+* */
+
 package org.hackSugar.hopper;
 
 import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Build;
+import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.content.Intent;
 import org.torproject.android.OrbotMainActivity;
+import org.torproject.android.service.TorService;
 
+
+/**
+ * @author Connor McDermid
+ * Contact me with issues at connor.mcdermid@mcvts.org
+ * This class builds a Quick Settings tile for use on the Android Quick Settings menu.
+ * The tile is set to the "Unavailable" state when the device is not unlocked, for security purposes,
+ * although this will not disable the Tor VPN.
+ */
 @TargetApi(Build.VERSION_CODES.N)
 public class HSTileService extends TileService {
+
+
+    Tile tile;
+
 
     @Override
     public void onClick() {
@@ -18,7 +38,6 @@ public class HSTileService extends TileService {
         Intent torStartIntent = new Intent(HSTileService.this, OrbotMainActivity.class);
         torStartIntent.setAction("org.torproject.android.START_TOR");
         startActivity(torStartIntent);
-
     }
 
     @Override
@@ -34,13 +53,34 @@ public class HSTileService extends TileService {
 
         //While I'm sure you get the point, for documentation purposes I must state that this is called when the tile
         // is added.
+
+        tile = getQsTile();
     }
 
     @Override
     public void onStartListening() {
         super.onStartListening();
 
+
         //This is called when the tile becomes visible.
+
+        if (isTorServiceRunning()) {
+            tile.setLabel("Tor");
+            tile.setContentDescription("Activates the Tor VPN");
+            if (isSecure()) {
+                tile.setState(Tile.STATE_UNAVAILABLE);
+                tile.updateTile();
+                return;
+            }
+            if (isTorServiceRunning()) {
+                tile.setState(Tile.STATE_ACTIVE);
+            } else {
+                tile.setState(Tile.STATE_INACTIVE);
+            }
+            tile.updateTile();
+        }
+
+
     }
 
     @Override
@@ -48,5 +88,15 @@ public class HSTileService extends TileService {
         super.onStopListening();
 
         //This is called when the tile is no longer visible.
+    }
+
+    private boolean isTorServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (TorService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
